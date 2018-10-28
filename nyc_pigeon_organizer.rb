808b16f6280bc4e14c27693d03b20db956ee3965
Linda Chang <lindachang186@gmail.com>
2018-10-28 13:44:02 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,13 @@ require "pry"
 def nyc_pigeon_organizer(data)
   pidgeons = {}
   data.each do |key, value|
+    if key == :gender 
     value.each do |attribute, pidgeon|
       pidgeon.each do |info|
         binding.pry
+
+          pidgeons << data[:gender][:male]
+
       end
     end
   end
